1dae2178afe1e7533c47f1f7068327f689b281ae
Mohammad Alipoor <mohammad.alpz@gmail.com>
2021-02-07 09:58:48 +0330

    nothing

--- lib/src/local_data_source/shared_pref_storage_client.dart
+++ lib/src/local_data_source/shared_pref_storage_client.dart
@@ -2,60 +2,35 @@ import 'package:flutter/foundation.dart';
 import 'package:hive/hive.dart';
 import 'package:idall_in_app_authentication/src/idall_strs.dart';
 
-// import 'package:shared_preferences/shared_preferences.dart';
-
 class SharedPrefStorageClient {
   Future<String> getValueFromSharedPref(String key) async {
     try {
-      if(!Hive.isBoxOpen(IdallStrs.hiveBox))
-      await  Hive.openBox(IdallStrs.hiveBox);
+      if (!Hive.isBoxOpen(IdallStrs.hiveBox))
+        await Hive.openBox(IdallStrs.hiveBox);
       var box = Hive.box(IdallStrs.hiveBox);
       return box.get(key);
-      // SharedPreferences pref = await SharedPreferences.getInstance();
-      // return pref.getString(key) ?? null;
     } catch (error, stackTrace) {
       debugPrint('error in get value from Storage $error , $stackTrace');
       return error;
     }
   }
 
-
-
   Future<void> setValueToSharedPref({String key, String value}) async {
     try {
-      if(!Hive.isBoxOpen(IdallStrs.hiveBox))
-       await Hive.openBox(IdallStrs.hiveBox);
+      if (!Hive.isBoxOpen(IdallStrs.hiveBox))
+        await Hive.openBox(IdallStrs.hiveBox);
       var box = Hive.box(IdallStrs.hiveBox);
       box.put(key, value);
-
-      // SharedPreferences pref = await SharedPreferences.getInstance();
-      // await pref.setString(key, value);
     } catch (error, stackTrace) {
       debugPrint('error in set value to Storage $error , $stackTrace');
       return error;
     }
   }
 
-  // Future<void> setListValueToSharedPref({String key, List<String> value}) async {
-  //   try {
-  //     SharedPreferences pref = await SharedPreferences.getInstance();
-  //     await pref.setStringList(key, value);
-  //   } catch (error, stackTrace) {
-  //     debugPrint('error in set value to Storage $error , $stackTrace');
-  //     return error;
-  //   }
-  // }
-
   Future<void> clearSharedPref({List<String> keys}) async {
     try {
       var box = Hive.box(IdallStrs.hiveBox);
       box.clear();
-      // SharedPreferences pref = await SharedPreferences.getInstance();
-      // if (keys != null && keys.isNotEmpty) {
-      //   keys.forEach((key) async {
-      //     await pref.remove(key);
-      //   });
-      // }
     } catch (error, stackTrace) {
       debugPrint('error in clear value from Storage $error , $stackTrace');
       return error;
